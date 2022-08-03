43e76730561b58796a1242d4e0b555d85e0c0e41
LARISSA MATTOS DE MARTINI <109092221+Larissa1799@users.noreply.github.com>
2022-08-02 23:20:48 -0300

    Add files via upload

--- Curso.java
+++ Curso.java
@@ -1,37 +1,28 @@
-package br.com.dio.desafio;
+package br.com.dio.desafio.dominio;
 
-public class Curso {
-private String titulo;
-private String descricao;
-private int cargaHoraria;
+public class Curso extends Conteudo{
 
-public Curso() {}
+	private int cargaHoraria;
+	
+	@Override 
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
+		
+	}
+	
+	public Curso() {
+		
+	}
 
-
-public String getTitulo() {
-	return titulo;
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	@Override
+	public String toString() {
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+	
 }
-public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
-public String getDescricao() {
-	return descricao;
-}
-public void setDescricao(String descricao) {
-	this.descricao = descricao;
-}
-public int getCargaHoraria() {
-	return cargaHoraria;
-}
-public void setCargaHoraria(int cargaHoraria) {
-	this.cargaHoraria = cargaHoraria;
-}
-@Override
-public String toString() {
-	return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
-}
-
-
-}
-
-
